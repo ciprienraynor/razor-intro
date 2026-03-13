856d00ed44958ada6142862dd8da6bfc335e323e
ciprienraynor <ciprien.raynor@gmail.com>
2026-03-13 16:52:12 +0100

    Start 02.Components.Composition

---
+++ 01.Components.Anatomy/Components/Pages/Home.razor.cs
@@ -0,0 +1,11 @@
+namespace _01.Components.Anatomy.Components.Pages;
+
+public partial class Home
+{
+    private int currentCount = 0;
+
+    private void IncrementCount()
+    {
+        currentCount++;
+    }
+}
